9c50de06e2e51a58f52a085f7daff52cefc70f41
Ye-Sheng Kuo <higherkuo@gmail.com>
2016-09-29 17:34:00 -0400

    initial commit for triumvi V10 not tested yet

---
+++ contiki/apps/triumviV10/project-conf.h
@@ -0,0 +1,43 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+/*
+ * @author: Brad Campbell <bradjc@umich.edu>
+ */
+
+// No need for UART
+#define STARTUP_CONF_VERBOSE 0
+
+//#ifdef ERASE_FRAM
+//#define RTC_SET
+//#define FRAM_WRITE
+
+//#define DATADUMP
+//#define DATADUMP2
+//#define DEBUG_ON
+
+//#define AVG_VREF      // calculate the reference by averaging a cycle
+
+#if defined(DATADUMP) || defined(DATADUMP2) || defined(DEBUG_ON)
+#define UART_CONF_ENABLE 1
+#endif
+
+#define POWERFACTOR_EN
+#define POLYFIT
+
+
+#define TRIUMVI_PKT_IDENTIFIER 0xa0
+#define LEN_LEN 2 // LVal
+#define MIC_LEN 4 // MVal
+#define ADATA_LEN 0x08
+#define PDATA_LEN 0x05
+
+#define VERSION10
+
+// Use custom address for this address
+// make ID=c0:98:e5:54:52:a0:00:01
+#define IEEE_ADDR_CONF_USE_SECONDARY_LOCATION 1
+
+#endif /* PROJECT_CONF_H_ */
+
+/** @} */
